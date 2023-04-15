aaa330dcb5a615dc88caf6c1d8c6940b095060c3
coderunway <accounts.raj@gmail.com>
2023-04-15 16:47:31 +0530

    Create installdocker.sh

---
+++ installdocker.sh
@@ -0,0 +1,18 @@
+################## 
+Installing Docker 
+##################
+
+#Remove any old versions 
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+#Downloading the install script 
+curl -fsSL https://get.docker.com -o get-docker.sh
+
+#performing Dry Run 
+#sudo sh ./get-docker.sh --dry-run
+
+#Installing the docker  
+sudo sh get-docker.sh
+
+#Installing the docker-compose 
+sudo apt-get install docker-compose
