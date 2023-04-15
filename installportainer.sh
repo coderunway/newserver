4a57bb1d57a2bcd6c7a3ba3acce046cf0c0120c5
coderunway <accounts.raj@gmail.com>
2023-04-15 16:48:53 +0530

    Create installportainer.sh

---
+++ installportainer.sh
@@ -0,0 +1,6 @@
+##################
+Install Portainer
+##################
+
+mkdir -p ~/workspace/portainer
+sudo docker run -p 8000:8000 -p 9000:9000 --detach --name=portainer-ce --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v ~/workspace/portainer:/data portainer/portainer-ce
