a33342264b5fb92d62d1293917f2b523c12b98c1
coderunway <accounts.raj@gmail.com>
2023-05-10 18:58:56 +0530

    Update installdocker.sh

--- installdocker.sh
+++ installdocker.sh
@@ -1,25 +1,25 @@
-echo ################## 
-echo # Installing Docker 
-echo ##################
+echo "##################" 
+echo "# Installing Docker" 
+echo "##################"
 echo 
 echo 
-echo ### --> Remove any old versions 
+echo "### --> Remove any old versions" 
 sudo apt-get remove docker docker-engine docker.io containerd runc
 
 echo
 echo 
-echo ### --> Downloading the install script 
+echo "### --> Downloading the install script" 
 curl -fsSL https://get.docker.com -o get-docker.sh
 
 #performing Dry Run 
 #sudo sh ./get-docker.sh --dry-run
 echo
 echo
-echo ### --> Installing the docker  
+echo "### --> Installing the docker"  
 sudo sh get-docker.sh
 
 echo
-echo ### --> Installing the docker-compose 
+echo "### --> Installing the docker-compose" 
 sudo apt-get install docker-compose
 echo
-echo 
+echo "## Completed..!!"
